ac66716bb2d45a7a707dcca1941ef17b641ac3e2
MOUCTARLAWAN <lawan.mouctar@ensi.ma>
2024-11-30 00:47:37 +0100

    Home page

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -29,6 +29,7 @@ const links = [
     }
 ]
 const MobileNav = () => {
+    const pathname = usePathname()
   return (
     <Sheet>
         <SheetTrigger className="flex items-center justify-center">
@@ -42,7 +43,17 @@ const MobileNav = () => {
                     </h1>
                 </Link>
             </div>
-            <nav>nav</nav>
+            <nav className="flex flex-col justify-center items-center gap-8">
+                {links.map((link, index)=> {
+                    return (
+                        <Link href={link.path} key={index} className={`${
+                            link.path === pathname && 
+                            "text-accent border-b-2 border-accent"
+                            } text-xl capitalize hover:text-accent transition-all`}>
+                            {link.name}
+                        </Link>)
+                })}
+            </nav>
         </SheetContent>
     </Sheet>
   )
