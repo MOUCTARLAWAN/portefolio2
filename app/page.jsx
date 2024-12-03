da29489683e33cdba09633594ae9ab0f4d1f5e7d
MOUCTARLAWAN <lawan.mouctar@ensi.ma>
2024-12-03 18:38:27 +0100

    reseaux sociaux

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,47 @@
-export default function Home() {
-  return <main>Home page</main>
+import Social from "@/components/Social";
+import { Button } from "@/components/ui/button"
+import { FiDownload } from 'react-icons/fi';
+
+
+const page = () => {
+  return (
+    <section className="h-full">
+      <div className="container mx-auto h-full">
+        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+          <div className="text-center xl:text-left">
+            <span className="text-xl">Ingénieur logiciel Big data & IA </span>
+            <h1 className="h1">
+              Hello, je suis <br /><span className="text-accent">LAWAN Mouctar Ali</span> 
+            </h1>
+            <p className="max-w-[500px] mb-9 text-white/80">
+            Ingénieur Logiciel Big Data et Intelligence Artificielle, 
+            je suis passionné par le développement de solutions innovantes et adaptées aux besoins technologiques actuels. 
+            Fort d'une expérience en conception et développement d'applications mobiles et web,
+            l'analyse des données.
+            </p>
+
+            {/* Boutton des reseaux sociaux */}
+           <div className="flex flex-col xl:flex-row items-center gap-8">
+           <Button
+              variant="online"
+              size="md"
+              className="uppercase flex items-center gap-2"
+            >
+              <span>Télécharger mon cv</span>
+              <FiDownload className="text-xl" />
+            </Button>
+            <div className="mb-8 xl:mb-0">
+              <Social containerStyle="flex gap-6" iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500" />
+            </div>
+           </div>
+          </div>
+
+            {/* Photo de profil */}
+          <div>Photo</div>
+        </div>
+      </div>
+    </section>
+  )
 }
+
+export default page
---
+++ components/Social.jsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+import { FaGithub, FaLinkedinIn } from "react-icons/fa";
+
+const socials = [
+  { icon: <FaGithub />, path: "https://github.com" },
+  { icon: <FaLinkedinIn />, path: "https://linkedin.com" },
+];
+
+
+const Social = ({ containerStyle, iconStyles }) => {
+  return (
+    <div className={containerStyle}>
+      {socials.map((item, index) => {
+        return (
+          <Link
+            key={index}
+            href={item.path}
+            className={iconStyles}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {item.icon}
+          </Link>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Social;
